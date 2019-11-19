a98d3c2ce5746280de6de1be87e2fc5dfaedd4d5
santiagolozada <santiagolozada95@gmail.com>
2019-11-18 21:19:58 -0500

    Create domain module

--- app/src/main/java/com/architect/coders/mu8/MainActivity.kt
+++ app/src/main/java/com/architect/coders/mu8/MainActivity.kt
@@ -2,11 +2,14 @@ package com.architect.coders.mu8
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.domain.DummyClass
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dummy = DummyClass("Santiago")
     }
 }
