efe4388be48c295d7a2a30467fd25300722a16b2
javier <javier.mcardona@gmail.com>
2019-12-01 23:19:47 -0500

    issue #1 Create Splash Activity se agrega coroutines-android al depencies, se crea interface Scope para manejar las coroutines, se crea el package splash y se implementa la activity y el viewmodel
    se realiza cambios solicitados para aprobación.
    se realiza cambios solicitados para aprobación.
    se cambian colores y se agrega tamaño fuente
    
    javier cardona <javier.mcardona@gmail.com>

---
+++ presentation/src/main/java/com/architect/coders/mu8/splash/SplashActivity.kt
@@ -0,0 +1,37 @@
+package com.architect.coders.mu8.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.architect.coders.mu8.MainActivity
+import com.architect.coders.mu8.R
+import com.architect.coders.mu8.splash.SplashViewModel.UiModel
+import com.architect.coders.mu8.utils.startActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: SplashViewModel
+    private lateinit var tvVersion: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        tvVersion = findViewById(R.id.tv_version)
+        viewModel = ViewModelProviders.of(this)[SplashViewModel::class.java]
+
+        viewModel.model.observe(this, Observer(::initUi))
+    }
+
+    private fun initUi(model: UiModel) {
+        when (model) {
+            is UiModel.GetVersion -> tvVersion.text = getString(R.string.version_name, model.versionName)
+            is UiModel.Navigation -> {
+                startActivity<MainActivity> {}
+                finish()
+            }
+        }
+    }
+}
